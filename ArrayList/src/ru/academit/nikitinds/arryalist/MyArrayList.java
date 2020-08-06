24906ac0c65891bcb634e13fd38a76a926ef8268
Dmitriy <sapa408@gmail.com>
2020-08-06 12:15:57 +0700

    сделал задачу ArrayList

---
+++ ArrayList/src/ru/academit/nikitinds/arryalist/MyArrayList.java
@@ -0,0 +1,344 @@
+package ru.academit.nikitinds.arryalist;
+
+import java.util.*;
+
+public class MyArrayList<E> implements List<E> {
+    private E[] elements;
+    private int length;
+    private int modCount;
+
+    public MyArrayList() {
+        //noinspection unchecked
+        elements = (E[]) new Object[10];
+    }
+
+    public MyArrayList(int initialCapacity) {
+        //noinspection unchecked
+        elements = (E[]) new Object[initialCapacity];
+    }
+
+    public MyArrayList(Collection<? extends E> c) {
+        if (c.size() != 0) {
+            //noinspection unchecked
+            elements = (E[]) c.toArray();
+            length = c.size();
+        } else {
+            //noinspection unchecked
+            elements = (E[]) new Object[10];
+        }
+    }
+
+    @Override
+    public E get(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Индекс " + index + " вышел за границы индексов списка. Размер списка " + length);
+        }
+
+        return elements[index];
+    }
+
+    @Override
+    public E set(int index, E element) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Индекс " + index + " вышел за границы индексов списка. Размер списка " + length);
+        }
+
+        E oldElement = elements[index];
+        elements[index] = element;
+        return oldElement;
+    }
+
+    @Override
+    public int size() {
+        return length;
+    }
+
+    public int capacity() {
+        return elements.length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void trimToSize() {
+        if (length < elements.length) {
+            elements = Arrays.copyOf(elements, length);
+        }
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        if (minCapacity > elements.length) {
+            elements = Arrays.copyOf(elements, minCapacity);
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new MyArrayListIterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elements, length);
+    }
+
+    public <T> T[] toArray(T[] a) {
+        if (a.length < length) {
+            a = Arrays.copyOf(a, length);
+        }
+
+        Object[] elements = this.elements;
+        Object[] newA = a;
+        System.arraycopy(elements, 0, newA, 0, length);
+
+        if (a.length > length) {
+            a[length] = null;
+        }
+
+        return a;
+    }
+
+    @Override
+    public boolean add(E e) {
+        add(length, e);
+        return true;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (index < 0 || index > length) {
+            throw new IndexOutOfBoundsException("Индекс " + index + " должен быть в диапазоне от 0 до " + length);
+        }
+
+        ensureCapacity(length + 1);
+
+        if (index == length) {
+            elements[length] = element;
+        } else {
+            System.arraycopy(elements, index, elements, index + 1, length - index);
+            elements[index] = element;
+        }
+
+        length++;
+        modCount++;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addAll(length, c);
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        if (index < 0 || index > length) {
+            throw new IndexOutOfBoundsException("Индекс " + index + " должен быть в диапазоне от 0 до " + length);
+        }
+
+        if (c.size() == 0) {
+            return false;
+        }
+
+        ensureCapacity(length + c.size());
+
+        if (index != length) {
+            System.arraycopy(elements, index, elements, index + c.size(), length - index);
+        }
+
+        Object[] addedCollection = c.toArray();
+        Object[] elements = this.elements;
+        System.arraycopy(addedCollection, 0, elements, index, addedCollection.length);
+        length += addedCollection.length;
+        modCount++;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < length; i++) {
+            if (Objects.equals(elements[i], o)) {
+                fastRemove(i);
+                length--;
+                modCount++;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Индекс " + index + " вышел за границы индексов списка. Размер списка " + length);
+        }
+
+        E removedElement = elements[index];
+        fastRemove(index);
+        length--;
+        modCount++;
+        return removedElement;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        if (c == null || c.size() == 0) {
+            return false;
+        }
+
+        boolean hasRemove = false;
+
+        for (int i = length - 1; i >= 0; i--) {
+            if (c.contains(elements[i])) {
+                fastRemove(i);
+                length--;
+                modCount++;
+                hasRemove = true;
+            }
+        }
+
+        return hasRemove;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        if (c == null || c.size() == 0) {
+            return false;
+        }
+
+        boolean hasRemove = false;
+
+        for (int i = length - 1; i >= 0; i--) {
+            if (!c.contains(elements[i])) {
+                fastRemove(i);
+                length--;
+                modCount++;
+                hasRemove = true;
+            }
+        }
+
+        return hasRemove;
+    }
+
+    private void fastRemove(int index) {
+        if (index < length - 1) {
+            System.arraycopy(elements, index + 1, elements, index, length - index - 1);
+        }
+
+        elements[length - 1] = null;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < length; i++) {
+            elements[i] = null;
+        }
+
+        length = 0;
+        modCount++;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        if (c == null || c.size() == 0) {
+            return false;
+        }
+
+        Object[] array = c.toArray();
+
+        for (Object o : array) {
+            if (!contains(o)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < length; i++) {
+            if (Objects.equals(elements[i], o)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = length - 1; i >= 0; i--) {
+            if (Objects.equals(elements[i], o)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        if (length == 0) {
+            return "[]";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder("[");
+
+        for (int i = 0; i < length - 1; i++) {
+            stringBuilder.append(elements[i]).append(", ");
+        }
+
+        return stringBuilder.append(elements[length - 1]).append("]").toString();
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        //noinspection ConstantConditions
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        //noinspection ConstantConditions
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        //noinspection ConstantConditions
+        return null;
+    }
+
+    private class MyArrayListIterator implements Iterator<E> {
+
+        private int currentIndex = -1;
+        private int expectedModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex + 1 < length;
+        }
+
+        @Override
+        public E next() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+
+            if (!hasNext()) {
+                throw new NoSuchElementException("В списке больше нет элементов");
+            }
+
+            currentIndex++;
+            return elements[currentIndex];
+        }
+    }
+}
---
+++ ArrayList/src/ru/academit/nikitinds/arryalist_main/Main.java
@@ -0,0 +1,79 @@
+package ru.academit.nikitinds.arryalist_main;
+
+import ru.academit.nikitinds.arryalist.MyArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList<Integer> integers = new MyArrayList<>(Arrays.asList(1, 2, 3, 4));
+        MyArrayList<String> strings = new MyArrayList<>();
+        MyArrayList<Number> numbers = new MyArrayList<>(5);
+
+        System.out.println("Список integers: " + integers);
+        System.out.println("Список strings: " + strings);
+        System.out.println("Список numbers: " + numbers);
+        System.out.println();
+
+        integers.ensureCapacity(7);
+        System.out.println("Увеличсилил вместимость integers, она теперь равна " + integers.capacity());
+        integers.trimToSize();
+        System.out.println("Уменьшили вместимость integers, она теперь равна " + integers.capacity());
+        System.out.println();
+
+        integers.set(0, integers.get(integers.size() - 1));
+        System.out.println("Заменили в integers элемент по индексу 0: " + integers);
+        System.out.println();
+
+        strings.add("Hello");
+        strings.add(strings.size(), "World!");
+        System.out.println("Добавили элементы в strings: " + strings);
+        System.out.println();
+
+        numbers.addAll(integers);
+        System.out.println("Добавили в numbers все элементы integers, numbers теперь содержит: " + numbers);
+        System.out.println();
+
+        numbers.remove(Integer.valueOf(4));
+        System.out.println("Удалили из numbers элемент 4: " + numbers);
+        numbers.remove(numbers.size() - 1);
+        System.out.println("Удалили из numbers элемент по индексу: " + numbers);
+        numbers.removeAll(integers);
+        System.out.println("Удалили из numbers все элементы, которые есть в integers, numbers теперь содержит: " + numbers);
+        System.out.println("Размер numbers равен " + numbers.size() + ", вместимость numbers равна " + numbers.capacity());
+        System.out.println();
+
+        numbers.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println("Добавили в numbers элементы, numbers теперь такой: " + numbers);
+        numbers.retainAll(integers);
+        System.out.println("Оставили в numbers только те элементы, которые есть в integers, numbers теперь такой: " + numbers);
+        System.out.println("Размер numbers равен " + numbers.size());
+        System.out.println();
+
+        if (numbers.containsAll(integers)) {
+            System.out.println("numbers содержит все элементы integers");
+        } else {
+            System.out.println("numbers не содержит все элементы integers");
+        }
+
+        String string = "Hello";
+
+        if (strings.contains(string)) {
+            System.out.println("strings содержит " + string);
+        } else {
+            System.out.println("numbers не содержит " + string);
+        }
+
+        System.out.println();
+
+        int number = 4;
+        System.out.println("Число " + number + " впервые встречается в integers по индексу " + integers.indexOf(number));
+        System.out.println("Число " + number + " в последний раз встречается в integers по индексу " + integers.lastIndexOf(number));
+        System.out.println();
+
+        if (!integers.isEmpty()) {
+            integers.clear();
+            System.out.println("Очистили integers: " + integers);
+            System.out.println("Размер integers равен " + integers.size() + ", вместимость integers равна " + integers.capacity());
+        }
+    }
+}
