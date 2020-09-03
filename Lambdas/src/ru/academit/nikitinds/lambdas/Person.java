09c805fb91774b0a667300a98f9b05e741051ab7
Dmitriy <sapa408@gmail.com>
2020-09-03 18:45:01 +0700

    сделал задачу Лямбды

---
+++ Lambdas/src/ru/academit/nikitinds/lambdas/Person.java
@@ -0,0 +1,43 @@
+package ru.academit.nikitinds.lambdas;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        if (name == null) {
+            throw new IllegalArgumentException("Имя не может быть null");
+        }
+
+        if (age <= 0 || age > 100) {
+            throw new IllegalArgumentException("Возраст должен быть больше 0 и меньше 100");
+        }
+
+        for (int i = 0; i < name.length(); i++) {
+            if (!Character.isLetter(name.charAt(i)) || (Character.getNumericValue(name.charAt(i)) >= 10 && Character.getNumericValue(name.charAt(i)) <= 35)) {
+                throw new IllegalArgumentException("Имя должно состоять только букв русского алфавита");
+            }
+
+            if (i == 0) {
+                if (Character.isLowerCase(name.charAt(i))) {
+                    throw new IllegalArgumentException("Имя должно начинаться с заглавной буквы");
+                }
+            } else {
+                if (Character.isUpperCase(name.charAt(i))) {
+                    throw new IllegalArgumentException("В имени заглавной должна быть только первая буква");
+                }
+            }
+        }
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ Lambdas/src/ru/academit/nikitinds/lambdas_main/Main.java
@@ -0,0 +1,63 @@
+package ru.academit.nikitinds.lambdas_main;
+
+import ru.academit.nikitinds.lambdas.Person;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<>(Arrays.asList(
+                new Person("Ольга", 31),
+                new Person("Ольга", 20),
+                new Person("Сергей", 28),
+                new Person("Дмитрий", 20),
+                new Person("Ирина", 30),
+                new Person("Максим", 50),
+                new Person("Максим", 5),
+                new Person("Елена", 17),
+                new Person("Игорь", 16),
+                new Person("Артём", 15)
+        ));
+
+        // задание А) получить список уникальных имен
+        List<String> uniqueNames = persons.stream()
+                .map(Person::getName)
+                .distinct()
+                .collect(Collectors.toList());
+
+        /* задание Б) вывести список уникальных имен в формате:
+        Имена: Иван, Сергей, Петр.*/
+        System.out.println(uniqueNames.stream()
+                .collect(Collectors.joining(", ", "Имена: ", "."))
+        );
+
+        // В) получить список людей младше 18, посчитать для них средний возраст
+        // можно так
+        System.out.printf("Средний возраст людей младше 18 лет равен %.2f%n", persons.stream()
+                .filter(p -> p.getAge() < 18)
+                .mapToDouble(Person::getAge)
+                .average()
+                .orElse(0)
+        );
+
+        //либо так
+        System.out.printf("Средний возраст людей младше 18 лет равен %.2f%n", persons.stream()
+                .filter(p -> p.getAge() < 18)
+                .collect(Collectors.averagingDouble(Person::getAge))
+        );
+
+        // Г) при помощи группировки получить Map, в котором ключи имена, а значения средний возраст
+        System.out.println(persons.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)))
+        );
+
+        // Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
+        System.out.println(persons.stream()
+                .filter(person -> person.getAge() >= 20 && person.getAge() <= 45)
+                .sorted((p1, p2) -> p2.getAge() - p1.getAge())
+                .map(Person::getName)
+                .collect(Collectors.joining(", ", "Имена людей от 20 до 45 лет в порядке убывания возраста: ", "."))
+        );
+    }
+}
