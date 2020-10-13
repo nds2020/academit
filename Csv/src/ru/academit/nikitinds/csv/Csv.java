0bc865b90675d7606e67a5243478b3ea4c7c946b
Dmitriy <sapa408@gmail.com>
2020-10-14 01:09:53 +0700

    Csv 1. Пути к файлам теперь берутся из аргументов программы 2. Добавил ; к литералам спецсимволов 3. Пустые строки во входном файле игнорируются 4. Добавил недостающие теги к структуре html-документа 5. Программа теперь выдает пользователю информативное сообщение при возниконовении исключения ввода-вывода.

--- Csv/src/ru/academit/nikitinds/csv/Csv.java
+++ Csv/src/ru/academit/nikitinds/csv/Csv.java
@@ -4,54 +4,68 @@ import java.io.*;
 
 public class Csv {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.csv")); PrintWriter writer = new PrintWriter("output.html")) {
+        if (args.length != 2) {
+            System.out.println("Для выполнения программы введите в указанном порядке, разделив пробелом, следующие параметры: " +
+                    "путь к CSV-файлу, который необходимо прочитать; " +
+                    "путь к HTML-файлу, в который необходимо сохранить результат выполнения программы");
+            System.exit(-1);
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0])); PrintWriter writer = new PrintWriter(args[1])) {
             writer.println("<!DOCTYPE html>");
             writer.println("<html lang=\"ru\">");
-            writer.println("<meta charset=\"utf-8\">");
-            writer.println("<title>Результат перевода таблицы из CSV в HTML</title>");
-            writer.println("<style>");
-            writer.println("  table {");
-            writer.println("    border: 1px solid black;");
-            writer.println("    border-collapse: collapse;");
-            writer.println("  }");
+            writer.println("  <head>");
+            writer.println("    <meta charset=\"utf-8\">");
+            writer.println("    <title>Результат перевода CSV в HTML</title>");
+            writer.println("    <style>");
+            writer.println("      table {");
+            writer.println("        border: 1px solid black;");
+            writer.println("        border-collapse: collapse;");
+            writer.println("      }");
             writer.println();
-            writer.println("  td {");
-            writer.println("    border: 1px solid black;");
-            writer.println("    padding: 0 .5em;");
-            writer.println("    vertical-align: top;");
-            writer.println("  }");
-            writer.println("</style>");
-            writer.println("<table>");
+            writer.println("      td {");
+            writer.println("        border: 1px solid black;");
+            writer.println("        padding: 0 .5em;");
+            writer.println("        vertical-align: top;");
+            writer.println("      }");
+            writer.println("    </style>");
+            writer.println("  </head>");
+            writer.println("  <body>");
+            writer.println("    <table>");
 
             boolean surroundedByQuotes = false;
             int innerQuotesCount = 0;
             String line;
 
             while ((line = reader.readLine()) != null) {
+                if (line.length() == 0) {
+                    continue;
+                }
+
                 if (!surroundedByQuotes) {
-                    writer.println("  <tr>");
-                    writer.print("    <td>");
+                    writer.println("      <tr>");
+                    writer.print("        <td>");
                 }
 
                 for (int i = 0; i < line.length(); i++) {
                     if (line.charAt(i) == '<') {
-                        writer.print("&lt");
+                        writer.print("&lt;");
                         continue;
                     }
 
                     if (line.charAt(i) == '>') {
-                        writer.print("&gt");
+                        writer.print("&gt;");
                         continue;
                     }
 
                     if (line.charAt(i) == '&') {
-                        writer.print("&amp");
+                        writer.print("&amp;");
                         continue;
                     }
 
                     if (line.charAt(i) == ',' && !surroundedByQuotes) {
                         writer.println("</td>");
-                        writer.print("    <td>");
+                        writer.print("        <td>");
                         continue;
                     }
 
@@ -83,13 +97,16 @@ public class Csv {
                     writer.print("<br>");
                 } else {
                     writer.println("</td>");
-                    writer.println("  </tr>");
+                    writer.println("      </tr>");
                 }
             }
 
-            writer.println("</table>");
+            writer.println("    </table>");
+            writer.println("  </body>");
+            writer.println("</html>");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.exit(1);
         }
     }
 }
